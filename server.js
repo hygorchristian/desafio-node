a6c57da3ef4c0012d01f6ca073c3f0d1b82e3547
Hygor Christian Dias <hygor.christian@gmail.com>
2019-07-01 20:35:03 -0300

    update

--- server.js
+++ server.js
@@ -21,6 +21,6 @@ const { Ignitor } = require('@adonisjs/ignitor')
 
 new Ignitor(require('@adonisjs/fold'))
   .appRoot(__dirname)
-  // .wsServer()
+  .wsServer()
   .fireHttpServer()
   .catch(console.error)
